942e6e4d29fff3b99b89495c06093b2f51342f67
Matt Russell <russell.d.matt@gmail.com>
2016-11-13 11:10:35 -0500

    improved the command-line stuff

--- black_scholes_cli/src/main.rs
+++ black_scholes_cli/src/main.rs
@@ -3,13 +3,21 @@ extern crate black_scholes;
 use getopts::Options;
 use std::env;
 
-fn print_usage(program: &str, opts: Options) {
+fn print_usage_and_exit(program: &str, opts: &Options, exit_code: i32) -> ! {
     let brief = format!("Usage: {} FILE [options]", program);
     print!("{}", opts.usage(&brief));
+    std::process::exit(exit_code);
+}
+
+fn print_usage_and_exit_if_help_present(program: &str, opts: &Options, m: &getopts::Matches) {
+    if m.opt_present("h") {
+        print_usage_and_exit(&program, opts, 0)
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // like sys.argv
+    let program = args[0].clone();
     fn add_help(mut opts: Options) -> Options { 
         opts.optflag("h", "help", "print this help menu");
         opts
@@ -18,24 +26,16 @@ fn main() {
     let opts = Options::new();
     let opts = black_scholes::PricingInput::add_opts(opts);
     let opts = add_help(opts);
-    { 
-        let just_help = Options::new();
-        let just_help = add_help(just_help);
-        match just_help.parse(&args[1..]) {
-            Err(_) => (), // no help
-            Ok(m) => { 
-                if m.opt_present("h") {
-                    let program = args[0].clone();
-                    print_usage(&program, opts);
-                    return;
-                }
-            }
-        }
-    }
-    
+
     let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => panic!(f.to_string()),
+        Ok(m) => { 
+            print_usage_and_exit_if_help_present(&program, &opts, &m);
+            m
+        },
+        Err(f) => {
+            println!("{}", f.to_string());
+            print_usage_and_exit(&program, &opts, 1)
+        },
     };
     let pricing_input = black_scholes::PricingInput::from_matches(matches);
     println!("{:?}", pricing_input);
